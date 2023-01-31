dc346616c89b0b2d3e5034975537367ac4004ce7
swapnil1305 <swapnil.satpute132@gmail.com>
2023-02-01 02:21:12 +0530

    Retrieving data from MySql

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'swappnil@7'
 });
 
 module.exports = pool.promise();
